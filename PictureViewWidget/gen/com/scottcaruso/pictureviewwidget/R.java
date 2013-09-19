18ebfb52f81dd7255d917ed7be191638d3e60635
Scott Caruso <scottcaruso@fullsail.edu>
2013-09-19 12:51:31 -0400

    Added image for widget app and for widget preview.

--- PictureViewWidget/gen/com/scottcaruso/pictureviewwidget/R.java
+++ PictureViewWidget/gen/com/scottcaruso/pictureviewwidget/R.java
@@ -27,6 +27,7 @@ public final class R {
         public static final int img3=0x7f020003;
         public static final int img4=0x7f020004;
         public static final int img5=0x7f020005;
+        public static final int photo=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f09000d;
